b9035aa24f092973b5e92d04055e48d0b5b52619
Wendell <wendellmorais.dev@gmail.com>
2024-09-16 17:45:05 -0300

    feat: add default value to courses in instructor model

--- Models/Instructor.cs
+++ Models/Instructor.cs
@@ -5,5 +5,5 @@ public class Instructor : BaseEntity
     public string Name { get; set; }
     public string Email { get; set; }
     public string Bio { get; set; }
-    public List<Course> Courses { get; set; }
+    public List<Course> Courses { get; set; } = new List<Course>();
 }
