09f4c4e621703f6801ffa45f86b3f0a2515e4db5
Wendell <wendellmorais.dev@gmail.com>
2024-09-18 14:13:21 -0300

    feat: set default value to student courses

--- Models/Student.cs
+++ Models/Student.cs
@@ -6,5 +6,5 @@ public class Student : BaseEntity
     public string Email { get; set; }
     public string Bio { get; set; }
     public DateOnly BirthDate { get; set; }
-    public List<Course> Courses { get; set; }
+    public List<Course> Courses { get; set; } = new List<Course>();
 }
